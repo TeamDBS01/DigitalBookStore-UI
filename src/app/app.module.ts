3ad73b481f4c5f9babdcddc1744816d5f2bd5d18
Sabarish <>
2025-04-08 11:05:04 +0530

    Add Font Awesome integration and light mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,19 @@ import { UserService } from 'src/user-customer/user/service/user.service';
 import { OrderService as adminOrderService } from 'src/user-admin/order/service/order.service';
 import { OrderService as customerOrderService } from 'src/user-customer/order/service/order.service';
 import { InventoryService } from 'src/user-admin/inventory/service/inventory.service';
+import { AllReviewsComponent } from 'src/user-customer/review/all-reviews/all-reviews.component';
+import { ReviewComponent } from 'src/user-customer/review/review/review.component';
+import { StarsComponent } from 'src/user-customer/review/stars/stars.component';
+import { AverageRatingComponent } from 'src/user-customer/review/average-rating/average-rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewAllReviewsComponent,
+    AllReviewsComponent,
+    ReviewComponent,
+    StarsComponent,
+    AverageRatingComponent,
   ],
   imports: [
     BrowserModule,
